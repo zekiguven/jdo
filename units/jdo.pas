0e5bb97a121c1a0560183fbec0320b71cce30b93
Silvio Clecio <silvioprog@gmail.com>
2012-05-31 17:54:26 -0300

    # Rename files and folders.

---
+++ units/jdo.pas
@@ -0,0 +1,59 @@
+(*
+  JSON Data Objects unit
+  Copyright (C) 2012-2014 Silvio Clecio.
+
+  https://github.com/silvioprog/jdo
+
+  All contributors:
+  Plase see the file CONTRIBUTORS, included in this distribution.
+
+  See the file LICENSE, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit JDO;
+
+{$I jdo.inc}
+
+interface
+
+uses
+  JDOConsts, Classes, SysUtils, SQLdb, DB, FPJSON, TypInfo;
+
+type
+  EJDOException = class(Exception)
+  public
+    constructor Create(AInstance: TObject; const AMsg: string);
+    constructor CreateFmt(AInstance: TObject; const AMsg: string;
+      const AArgs: array of const);
+  end;
+
+{$i jdoconfigh.inc}
+{$i jdoqueryh.inc}
+{$i jdosqlh.inc}
+{$i jdodatabaseh.inc}
+
+implementation
+
+constructor EJDOException.Create(AInstance: TObject; const AMsg: string);
+begin
+  inherited CreateFmt(ERROR_MASK, [AInstance.ClassName, AMsg]);
+end;
+
+constructor EJDOException.CreateFmt(AInstance: TObject; const AMsg: string;
+  const AArgs: array of const);
+begin
+  inherited CreateFmt(Format(ERROR_MASK, [AInstance.ClassName, AMsg]), AArgs);
+end;
+
+{$i jdoconfig.inc}
+{$i jdoquery.inc}
+{$i jdosql.inc}
+{$i jdodatabase.inc}
+
+end.
+
---
+++ units/jdoconsts.pas
@@ -0,0 +1,75 @@
+(*
+  JSON Data Objects, consts unit
+  Copyright (C) 2012-2014 Silvio Clecio.
+
+  https://github.com/silvioprog/jdo
+
+  All contributors:
+  Plase see the file CONTRIBUTORS, included in this distribution.
+
+  See the file LICENSE, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit JDOConsts;
+
+{$I jdo.inc}
+
+interface
+
+const
+  ES = '';
+  SP = #32;
+  PT = #37; // %
+  AK = #42; // *
+  CO = #58; // :
+  CS = #44; // ,
+  DT = #46; // .
+  BS = #91; // [
+  BE = #93; // ]
+  EQ = #61; // =
+  PE = #41; // )
+  PS = #40; // (
+  PO = #35; // #
+  SC = #59; // ;
+  DEFAULT_PRIMARY_KEY = 'id';
+  CONNECTOR_TYPE = 'connectortype';
+  SQL_SELECT_TOKEN = 'select ';
+  SQL_FROM_TOKEN = 'from ';
+  SQL_WHERE_TOKEN = ' where ';
+  SQL_ORDER_BY_TOKEN = 'order by ';
+  SQL_INSERT_TOKEN = 'insert into ';
+  SQL_VALUES_TOKEN = ' values ';
+  SQL_UPDATE_TOKEN = 'update ';
+  SQL_SET_TOKEN = ' set ';
+  SQL_DELETE_TOKEN = 'delete ';
+  SQL_EQ_PARAM_TOKEN = '=:';
+  SQL_LOWER_TOKEN = 'lower';
+  SQL_LIKE_TOKEN = ' like (:';
+  FT_NULL = 'null';
+  FT_STRING = 'string';
+  FT_BOOLEAN = 'boolean';
+  FT_DATE = 'date';
+  FT_FLOAT = 'float';
+  FT_INT = 'int';
+  ERROR_MASK = '%s: %s';
+  NullDate = 0;
+
+  // Error msgs
+  SKeyEmptyError = '"Key" must not be empty.';
+  SConnTypeEmptyError = '"ConnectorType" must not be empty.';
+  SConnUnitWasNotDeclaredError = 'The unit of "%s" was not declared in uses clause or it is an invalid ConnectorType.';
+  SCfgFileNotFoundError = 'Config file not found: "%s"';
+  STargetNilError = '"Target" must not be nil.';
+  STableNameEmptyError = '"TableName" must be not empty.';
+  SFieldDefsNilError = '"FieldDefs" must be not nil.';
+  SFieldDefNotFoundError = 'FieldDef not found: "%s".';
+
+implementation
+
+end.
+
