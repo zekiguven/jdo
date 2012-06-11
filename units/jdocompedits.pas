b0daa95800fe7e955debc086259874319ec8915d
Silvio Clecio <silvioprog@gmail.com>
2012-06-10 21:20:45 -0300

    Adds designtime package.

---
+++ units/jdocompedits.pas
@@ -0,0 +1,87 @@
+(*
+  JDO component editors unit
+  Copyright (C) 2012-2014 Silvio Clecio.
+
+  https://github.com/silvioprog/jdo
+
+  All contributors:
+  Plase see the file CONTRIBUTORS, included in this distribution.
+
+  See the file LICENSE, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit JDOCompEdits;
+
+{$I jdo.inc}
+
+interface
+
+uses
+  JDO, ComponentEditors, PropEdits, Dialogs, Controls;
+
+type
+  TJDOSQLComponentEditor = class(TDefaultComponentEditor)
+  public
+    procedure ExecuteVerb(AIndex: Integer); override;
+    function GetVerb(AIndex: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+implementation
+
+procedure TJDOSQLComponentEditor.ExecuteVerb(AIndex: Integer);
+var
+  VSQL: TJDOSQL;
+  VHook: TPropertyEditorHook;
+begin
+  case AIndex of
+    0: inherited;
+    1:
+      begin
+        if MessageDlg('Generate SQL statements?', mtConfirmation,
+          mbYesNo, 0) <> mrYes then
+          Exit;
+        GetHook(VHook);
+        VSQL := GetComponent as TJDOSQL;
+        with VSQL do
+        begin
+          if not Assigned(VSQL.Query) then
+            Exit;
+          try
+            Query.Close;
+            Compose(jstSelect);
+            Query.Open;
+            Query.Close;
+            Compose(jstInsert);
+            Compose(jstUpdate);
+            Compose(jstDelete);
+          finally
+            Reset;
+          end;
+        end;
+        if Assigned(VHook) then
+          VHook.Modified(Self);
+        ShowMessage('SQL statements are generated successfully!');
+      end;
+  end;
+end;
+
+function TJDOSQLComponentEditor.GetVerb(AIndex: Integer): string;
+begin
+  case AIndex of
+    0: Result := inherited GetVerb(AIndex);
+    1: Result := 'Generate SQL statements ...';
+  end;
+end;
+
+function TJDOSQLComponentEditor.GetVerbCount: Integer;
+begin
+  Result := 2;
+end;
+
+end.
---
+++ units/jdoreg.pas
@@ -0,0 +1,46 @@
+(*
+  JDO register components unit
+  Copyright (C) 2012-2014 Silvio Clecio.
+
+  https://github.com/silvioprog/jdo
+
+  All contributors:
+  Plase see the file CONTRIBUTORS, included in this distribution.
+
+  See the file LICENSE, included in this distribution,
+  for details about the copyright.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*)
+
+unit JDOReg;
+
+{$I jdo.inc}
+
+interface
+
+uses
+  JDO, JDOCompEdits, Classes, ComponentEditors, PropEdits, SQLdb;
+
+procedure Register;
+
+implementation
+
+procedure Register;
+begin
+  RegisterComponents('JDO', [TJDOConfigurator, TJDOSQL, TJDODataBase,
+    TJDOQuery]);
+  RegisterComponentEditor(TJDOSQL, TJDOSQLComponentEditor);
+  RegisterPropertyEditor(TypeInfo(string), TJDOConfigurator,
+    'Configuration', TFileNamePropertyEditor);
+  RegisterPropertyEditor(TypeInfo(string), TJDODataBase,
+    'Configuration', TFileNamePropertyEditor);
+  RegisterPropertyEditor(TypeInfo(TSQLTransaction), TJDODataBase,
+    'Transaction', THiddenPropertyEditor);
+end;
+
+end.
+
+
